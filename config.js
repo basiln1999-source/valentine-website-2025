c91c47db1eae8a0d9fcffb48727a130ecbcef3d5
basiln1999-source <basiln.1999@icloud.com>
2026-01-28 15:35:58 -0600

    Update config.js

--- config.js
+++ config.js
@@ -22,13 +22,13 @@ const CONFIG = {
     // Customize each question and its possible responses
     questions: {
         first: {
-            text: "Biggie do you like me?",                                    // First interaction
+            text: "Biggie do you know how much i like you?",                                    // First interaction
             yesBtn: "Yes",                                             // Text for "Yes" button
             noBtn: "No",                                               // Text for "No" button
             secretAnswer: "I don't like you, I love you! ❤️"           // Secret hover message
         },
         second: {
-            text: "How much do you love me?",                          // For the love meter
+            text: "How much you think i love you?",                          // For the love meter
             startText: "This much!",                                   // Text before the percentage
             nextBtn: "Next ❤️"                                         // Text for the next button
         },
@@ -42,9 +42,9 @@ const CONFIG = {
     // Love meter messages
     // They show up depending on how far they slide the meter
     loveMessages: {
-        extreme: "WOOOOW You love me that much?? 🥰🚀💝",  // Shows when they go past 5000%
-        high: "To infinity and beyond! 🚀💝",              // Shows when they go past 1000%
-        normal: "And beyond! 🥰"                           // Shows when they go past 100%
+        extreme: "thats less than 1% of my love to you 🥰🚀💝",  // Shows when they go past 5000%
+        high: "Nigga way more than this too! 🚀💝",              // Shows when they go past 1000%
+        normal: "girl zfuck i love you way more"                           // Shows when they go past 100%
     },
 
     // Messages that appear after they say "Yes!"
